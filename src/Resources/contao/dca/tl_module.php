d428d64d6d63d7802484bf62a16b86e27c834d88
Marko Cupic <m.cupic@gmx.ch>
2018-10-04 11:49:44 +0200

    dev

---
+++ src/Resources/contao/dca/tl_module.php
@@ -0,0 +1,14 @@
+<?php
+// Palette
+$GLOBALS['TL_DCA']['tl_module']['palettes']['memberPictureFeed'] = '{title_legend},name,headline,type;{upload_folder_settings},memberPictureFeedUploadFolder;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID';
+
+
+// Fields
+$GLOBALS['TL_DCA']['tl_module']['fields']['memberPictureFeedUploadFolder'] = array(
+    'label'     => &$GLOBALS['TL_LANG']['tl_module']['memberPictureFeedUploadFolder'],
+    'exclude'   => true,
+    'inputType' => 'fileTree',
+    'eval'      => array('fieldType' => 'radio', 'filesOnly' => false, 'mandatory' => true, 'tl_class' => 'clr'),
+    'sql'       => "binary(16) NULL"
+);
+ 
