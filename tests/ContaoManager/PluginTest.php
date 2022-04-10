d3096a7b4ca92a78a1ff1d9ae8a26e9d34934280
Marko Cupic <m.cupic@gmx.ch>
2022-04-10 21:16:56 +0200

    Update class phpdoc

--- tests/ContaoManager/PluginTest.php
+++ tests/ContaoManager/PluginTest.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of Member Picture Feed Bundle.
+ * This file is part of Member Picture Feed.
  *
  * (c) Marko Cupic 2022 <m.cupic@gmx.ch>
  * @license GPL-3.0-or-later
